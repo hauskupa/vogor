34807df31cd3293c2804c52a488af305aa8411b8
hauskupa <shakon@shakon.com>
2025-11-11 18:13:52 +0000

    Auto deploy $(date +'%Y-%m-%d %H:%M:%S')

---
+++ dist/main.js
@@ -0,0 +1,3 @@
+// Build version: 832347 (2025-11-11T18:13:52.347Z)
+(()=>{console.log("test-2025-09-24 from src/main.js");document.addEventListener("DOMContentLoaded",()=>{});})();
+//# sourceMappingURL=main.js.map
---
+++ src/main.js
@@ -0,0 +1,6 @@
+console.log("test-2025-09-24 from src/main.js");
+
+
+document.addEventListener("DOMContentLoaded", () => {
+ 
+});
